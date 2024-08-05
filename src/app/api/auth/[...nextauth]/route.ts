840694543fce186b4253a91b99ea4ee5e89fd9e9
HasanBS <hasanbasrisenkul@gmail.com>
2024-08-05 20:41:54 +0300

    mongo, google auth and aws s3 connection created and packages added

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,46 @@
+import client from "@/app/libs/mongoConnect";
+import { User } from "@/app/models/User";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import bcrypt from "bcrypt";
+import * as mongoose from "mongoose";
+import NextAuth, {getServerSession} from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions = {
+  secret: process.env.SECRET,
+  adapter: MongoDBAdapter(client),
+  providers: [
+  GoogleProvider({
+    clientId: process.env.GOOGLE_CLIENT_ID??'',
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET??''
+  }),
+    CredentialsProvider({
+      name: 'Credentials',
+      id: 'credentials',
+      credentials: {
+        email: { label: "Email", type: "email", placeholder: "test@example.com" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        
+        const email = credentials?.email;
+        const password = credentials?.password;
+        mongoose.connect(process.env.MONGODB_URI??'');
+
+        const user = await User.findOne({email});
+        const passwordOk = user && bcrypt.compareSync(password ?? '', user.password);
+
+        if (passwordOk) {
+          return user;
+        }
+        return null;
+      }
+    })
+  ],
+};
+
+
+const handler = NextAuth(authOptions as any);
+
+export { handler as GET, handler as POST }
