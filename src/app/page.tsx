3b037219570be4966578d2fe160d0feec88bff6f
Furkan Tokmak <furukaan@gmail.com>
2024-08-11 21:32:08 +0300

    Layout added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
-import Hero from './components/layout/Hero';
 export default function Home() {
   return (
     <div>
-      <Hero />
       <div className="flex items-center justify-center">
         <h1 className="text-4xl font-bold mt-48">Welcome to Next Starter Kit</h1>
       </div>
