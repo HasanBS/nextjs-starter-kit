ed30b3339510188bb5708321b6e4dc7776a7be16
Furkan Tokmak <furukaan@gmail.com>
2024-08-17 19:12:22 +0300

    refactor

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,51 +1,49 @@
-import client from "@/lib/mongoConnect";
-import { User } from "@/models/User";
-import { MongoDBAdapter } from "@auth/mongodb-adapter";
-import bcrypt from "bcrypt";
-import * as mongoose from "mongoose";
-import NextAuth, {getServerSession} from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import GoogleProvider from "next-auth/providers/google";
-import GitHubProvider from "next-auth/providers/github";
+import client from '@/lib/mongoConnect';
+import { User } from '@/models/User';
+import { MongoDBAdapter } from '@auth/mongodb-adapter';
+import bcrypt from 'bcrypt';
+import * as mongoose from 'mongoose';
+import NextAuth, { getServerSession } from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import GoogleProvider from 'next-auth/providers/google';
+import GitHubProvider from 'next-auth/providers/github';
 
 export const authOptions = {
-  secret: process.env.SECRET,
-  adapter: MongoDBAdapter(client),
-  providers: [
-  GoogleProvider({
-    clientId: process.env.GOOGLE_CLIENT_ID ?? '',
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ''
-  }),
-  GitHubProvider({
-    clientId: process.env.GITHUB_ID ?? '',
-    clientSecret: process.env.GITHUB_SECRET ?? ''
-  }),
-    CredentialsProvider({
-      name: 'Credentials',
-      id: 'credentials',
-      credentials: {
-        email: { label: "Email", type: "email", placeholder: "test@example.com" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials, req) {
-        
-        const email = credentials?.email;
-        const password = credentials?.password;
-        mongoose.connect(process.env.MONGODB_URI ?? '');
+    secret: process.env.NEXTAUTH_SECRET,
+    adapter: MongoDBAdapter(client),
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID ?? '',
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+        }),
+        GitHubProvider({
+            clientId: process.env.GITHUB_ID ?? '',
+            clientSecret: process.env.GITHUB_SECRET ?? '',
+        }),
+        CredentialsProvider({
+            name: 'Credentials',
+            id: 'credentials',
+            credentials: {
+                email: { label: 'Email', type: 'email', placeholder: 'test@example.com' },
+                password: { label: 'Password', type: 'password' },
+            },
+            async authorize(credentials, req) {
+                const email = credentials?.email;
+                const password = credentials?.password;
+                mongoose.connect(process.env.MONGODB_URI ?? '');
 
-        const user = await User.findOne({email});
-        const passwordOk = user && bcrypt.compareSync(password ?? '', user.password);
+                const user = await User.findOne({ email });
+                const passwordOk = user && bcrypt.compareSync(password ?? '', user.password);
 
-        if (passwordOk) {
-          return user;
-        }
-        return null;
-      }
-    })
-  ],
+                if (passwordOk) {
+                    return user;
+                }
+                return null;
+            },
+        }),
+    ],
 };
-
 
 const handler = NextAuth(authOptions as any);
 
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
