b59e55905753d6b21e06acd7ecead3d705716790
Furkan Tokmak <furukaan@gmail.com>
2024-08-30 07:10:49 +0300

    table progress

--- src/components/ui/menu-table/menu-table.tsx
+++ src/components/ui/menu-table/menu-table.tsx
@@ -2,6 +2,9 @@
 
 import React, { useEffect, useState } from 'react';
 import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, getKeyValue } from '@nextui-org/table';
+import { Tooltip } from '@nextui-org/tooltip';
+import { Delete, Edit, Eye, Trash } from 'lucide-react';
+import Link from 'next/link';
 
 type Column = {
     key: string;
@@ -19,14 +22,40 @@ export function MenuTable() {
     useEffect(() => {
         const fetchData = () => {
             fetch('/api/menu')
-                .then(response => response.json())
-                .then(data => setRows(data))
-                .catch(error => console.error('Error fetching menu data:', error));
+                .then((response) => response.json())
+                .then((data) => setRows(data))
+                .catch((error) => console.error('Error fetching menu data:', error));
         };
 
         fetchData();
     }, []);
-    
+
+    const renderCell = React.useCallback((menu: any, columnKey: any) => {
+        const cellValue = menu[columnKey];
+
+        switch (columnKey) {
+            case 'actions':
+                return (
+                    <div className="relative flex items-center gap-2">
+                        <Tooltip content="Edit menu">
+                            <Link href={'/menu/' + menu._id}>
+                                <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
+                                    <Edit />
+                                </span>
+                            </Link>
+                        </Tooltip>
+                        <Tooltip color="danger" content="Delete menu">
+                            <span className="text-lg text-danger cursor-pointer active:opacity-50">
+                                <Trash />
+                            </span>
+                        </Tooltip>
+                    </div>
+                );
+            default:
+                return cellValue;
+        }
+    }, []);
+
     return (
         <Table>
             <TableHeader>
@@ -36,7 +65,7 @@ export function MenuTable() {
             </TableHeader>
             <TableBody>
                 {rows.map((row: any) => (
-                    <TableRow key={row._id}>{(columnKey) => <TableCell>{getKeyValue(row, columnKey)}</TableCell>}</TableRow>
+                    <TableRow key={row._id}>{(columnKey) => <TableCell>{renderCell(row, columnKey)}</TableCell>}</TableRow>
                 ))}
             </TableBody>
         </Table>
--- src/middleware.ts
+++ src/middleware.ts
@@ -5,25 +5,25 @@ import { getToken } from 'next-auth/jwt';
 const secret = process.env.NEXTAUTH_SECRET;
 
 export async function middleware(req: NextRequest) {
-    const { pathname } = req.nextUrl;
+    // const { pathname } = req.nextUrl;
 
-    if (pathname.startsWith('/_next/') || pathname.startsWith('/api/auth/') || pathname === '/favicon.ico') {
-        return NextResponse.next();
-    }
+    // if (pathname.startsWith('/_next/') || pathname.startsWith('/api/auth/') || pathname === '/favicon.ico') {
+    //     return NextResponse.next();
+    // }
 
-    const token = await getToken({ req, secret });
-    if (pathname.startsWith('/login') || pathname.startsWith('/register') || pathname === '/') {
-        if (token) {
-            const dashBoardUrl = new URL('/dashboard', req.url);
-            return NextResponse.redirect(dashBoardUrl);
-        }
-        return NextResponse.next();
-    } 
+    // const token = await getToken({ req, secret });
+    // if (pathname.startsWith('/login') || pathname.startsWith('/register') || pathname === '/') {
+    //     if (token) {
+    //         const dashBoardUrl = new URL('/dashboard', req.url);
+    //         return NextResponse.redirect(dashBoardUrl);
+    //     }
+    //     return NextResponse.next();
+    // } 
 
-    if (!token) {
-        const loginUrl = new URL('/login', req.url);
-        return NextResponse.redirect(loginUrl);
-    }
+    // if (!token) {
+    //     const loginUrl = new URL('/login', req.url);
+    //     return NextResponse.redirect(loginUrl);
+    // }
     
-    return NextResponse.next();
+    // return NextResponse.next();
 }
