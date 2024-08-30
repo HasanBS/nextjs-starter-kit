7b2f9f4608081da0e4499d31716be5572bf7b32f
Furkan Tokmak <furukaan@gmail.com>
2024-08-30 05:01:15 +0300

    next ui theme

--- src/app/(manager)/layout.tsx
+++ src/app/(manager)/layout.tsx
@@ -1,22 +1,35 @@
+"use client";
+
 import type { Metadata } from 'next';
 import { Roboto } from 'next/font/google';
 import SideBar from '@/components/ui/layout/side-bar';
 import { Header } from '@/components/ui/layout/header';
+import { useEffect, useState } from 'react';
+import { cn } from '@/lib/utils';
 
 const roboto = Roboto({ subsets: ['latin'], weight: ['400', '500', '700'] });
-
-export const metadata: Metadata = {
-    title: 'Next.JS Starter Kit',
-    description: 'Next.JS Starter Kit with Tailwind CSS and TypeScript. It includes authentication, API routes, and more.',
-};
 
 export default function Layout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
+    const [theme, setTheme] = useState<'dark' | 'light'>('light');
+
+    useEffect(() => {
+        const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+        setTheme(mediaQuery.matches ? 'dark' : 'light');
+
+        const handleChange = (e: MediaQueryListEvent) => {
+            setTheme(e.matches ? 'dark' : 'light');
+        };
+
+        mediaQuery.addEventListener('change', handleChange);
+        return () => mediaQuery.removeEventListener('change', handleChange);
+    }, []);
+    
     return (
-        <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+        <div className={cn("grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]", theme)}>
             <SideBar />
             <div className="flex flex-col">
                 <Header />
