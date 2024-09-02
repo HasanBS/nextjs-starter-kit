b8bf088eee731d98c179e91844fdd68f9e293e3a
Furkan Tokmak <furukaan@gmail.com>
2024-09-02 21:43:50 +0300

    delete chip

--- src/app/(manager)/menus/page.tsx
+++ src/app/(manager)/menus/page.tsx
@@ -6,11 +6,9 @@ import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, Di
 import { MenuItemForm } from '@/components/ui/form/menu-item-form';
 import { MenuForm } from '@/components/ui/form/menu-form';
 import { MenuTable } from '@/components/ui/menu-table/menu-table';
-import { Chip } from "@nextui-org/chip";
 import toast from 'react-hot-toast';
 import { IMenu } from '@/models/interfaces/IMenu';
 import { useEffect, useState } from 'react';
-
 
 export default function Page() {
     const [menus, setMenus] = useState([] as IMenu[]);
@@ -44,58 +42,50 @@ export default function Page() {
             method: 'GET',
         }).then(async (res) => {
             if (res.ok) {
-                return res.json().then(data => {
+                return res.json().then((data) => {
                     setMenus(data);
                 });
             } else {
                 toast.error('Menu items could not be fetched');
             }
-        }
-        )
+        });
     }
 
     return (
-        <section className='items-center'>
-            {menus.map((menu, index) => (
-                <Chip key={index} className=' text-gray-400 border-white' onClose={() => handleClose(menu)} variant="flat">
-                    {menu.name}
-                </Chip>
-            ))}
-            <div className="w-full items-center flex-col gap-4">
-                <div className="ml-auto flex items-center gap-2">
+        <div className="w-full items-center flex-col gap-4">
+            <div className="ml-auto flex items-center gap-2">
                 <Dialog>
-                        <DialogTrigger asChild>
-                            <Button size="sm" className="h-8 gap-1">
-                                <PlusCircle className="h-3.5 w-3.5" />
-                                <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">Add Product</span>
-                            </Button>
-                        </DialogTrigger>
-                        <DialogContent className="sm:max-w-[425px]">
-                            <DialogHeader>
-                                <DialogTitle>Add new product</DialogTitle>
-                                <DialogDescription>Add your product here. Click submit when you&apos;re done.</DialogDescription>
-                            </DialogHeader>
-                            <MenuItemForm></MenuItemForm>
-                        </DialogContent>
-                    </Dialog>
-                    <Dialog>
-                        <DialogTrigger asChild>
-                            <Button size="sm" className="h-8 gap-1">
-                                <PlusCircle className="h-3.5 w-3.5" />
-                                <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">Add menu</span>
-                            </Button>
-                        </DialogTrigger>
-                        <DialogContent className="sm:max-w-[425px]">
-                            <DialogHeader>
-                                <DialogTitle>Add new product</DialogTitle>
-                                <DialogDescription>Add your product here. Click submit when you&apos;re done.</DialogDescription>
-                            </DialogHeader>
-                            <MenuForm></MenuForm>
-                        </DialogContent>
-                    </Dialog>
-                </div>
-                <MenuTable></MenuTable>
+                    <DialogTrigger asChild>
+                        <Button size="sm" className="h-8 gap-1">
+                            <PlusCircle className="h-3.5 w-3.5" />
+                            <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">Add Product</span>
+                        </Button>
+                    </DialogTrigger>
+                    <DialogContent className="sm:max-w-[425px]">
+                        <DialogHeader>
+                            <DialogTitle>Add new product</DialogTitle>
+                            <DialogDescription>Add your product here. Click submit when you&apos;re done.</DialogDescription>
+                        </DialogHeader>
+                        <MenuItemForm></MenuItemForm>
+                    </DialogContent>
+                </Dialog>
+                <Dialog>
+                    <DialogTrigger asChild>
+                        <Button size="sm" className="h-8 gap-1">
+                            <PlusCircle className="h-3.5 w-3.5" />
+                            <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">Add menu</span>
+                        </Button>
+                    </DialogTrigger>
+                    <DialogContent className="sm:max-w-[425px]">
+                        <DialogHeader>
+                            <DialogTitle>Add new product</DialogTitle>
+                            <DialogDescription>Add your product here. Click submit when you&apos;re done.</DialogDescription>
+                        </DialogHeader>
+                        <MenuForm></MenuForm>
+                    </DialogContent>
+                </Dialog>
             </div>
-        </section>
+            <MenuTable></MenuTable>
+        </div>
     );
 }
--- src/models/Menu.ts
+++ src/models/Menu.ts
@@ -1,14 +1,15 @@
 import { Model, model, models, Schema, Types } from 'mongoose';
 
 interface IMenu {
+  tenantId: Types.ObjectId;
   name: string;
   menuItems: Types.ObjectId[];
 }
 
-const MenuSchema: Schema = new Schema({
-  tenantId: { type: Types.ObjectId, required: true, index: true },
+const MenuSchema = new Schema<IMenu>({
+  tenantId: { type: Schema.Types.ObjectId, required: true, index: true },
   name: { type: String, required: true },
   menuItems: [{ type: Schema.Types.ObjectId, ref: 'MenuItem' }],
-}, { timestamps: true });
+});
 
 export const Menu: Model<IMenu> = models?.Menu || model<IMenu>('Menu', MenuSchema);
