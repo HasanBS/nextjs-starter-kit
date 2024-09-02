72927c7d4188bcc5b8da10ff6c4071ba99f5ab70
Furkan Tokmak <furukaan@gmail.com>
2024-09-02 20:57:32 +0300

    add menu delete

--- src/app/api/menu/[id]/route.ts
+++ src/app/api/menu/[id]/route.ts
@@ -26,3 +26,30 @@ export async function GET(req: Request, {params}: {params: {id: string}}) {
         }
     });
 }
+
+
+export async function DELETE(req: Request, {params}: {params: {id: string}}) {
+    if (!mongoose.connection.readyState) {
+        console.log('Connecting to database');
+        try {
+            await mongoose.connect(process.env.MONGODB_URI ?? '');
+        } catch (err) {
+            console.error('Failed to connect to database', err);
+            return new Response(JSON.stringify({ error: 'Failed to connect to database' }), {
+                status: 500,
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+        }
+    }
+
+    const menu = await Menu.findOneAndDelete({_id: params.id});
+
+    return new Response(JSON.stringify(menu), {
+        status: 200,
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+}
