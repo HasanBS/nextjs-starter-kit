e84b6e64757f52953e0e96daf92fb87617d1c8de
Furkan Tokmak <furukaan@gmail.com>
2024-09-04 01:07:24 +0300

    some omit things

--- src/app/(manager)/menus/[id]/page.tsx
+++ src/app/(manager)/menus/[id]/page.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import MenuConfigurator from './_components/menu-configurator';
 import mongoose, { InferSchemaType } from 'mongoose';
-import { Menu } from '@/models/Menu';
+import { Menu, MenuType } from '@/models/Menu';
 import { MenuItemType } from '@/models/MenuItem';
 
-type MenuType = InferSchemaType<typeof Menu.schema>;
 export default async function Page({ params }: { params: { id: string } }) {
     if (!mongoose.connection.readyState) {
         await mongoose.connect(process.env.MONGODB_URI ?? '');
     }
 
-    const menu = await Menu.findOne({ _id: params.id }).populate<{menuItems: MenuItemType[]}>('menuItems').lean();
-    
+    const menu = await Menu.findOne({ _id: params.id }).populate<{menuItems: MenuItemType[]}>('menuItems').lean().exec();
+
+    const plainMenu: Omit<MenuType, 'menuItems'> & {menuItems: MenuItemType[]} = JSON.parse(JSON.stringify(menu));
+
     return (
         <>
-            <MenuConfigurator menu={menu} />
+            <MenuConfigurator menu={plainMenu} />
         </>
     );
 }
